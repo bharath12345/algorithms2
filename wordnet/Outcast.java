a43fb957d45ab76f0447f27ad71b94fa1795b347
bharath12345 <bharath12345@gmail.com>
2013-11-07 14:52:36 +0530

    results match... I guess its solved

--- wordnet/Outcast.java
+++ wordnet/Outcast.java
@@ -10,14 +10,32 @@ import stdlib.StdOut;
  */
 public class Outcast {
 
+    private WordNet wordnet;
+
     // constructor takes a WordNet object
     public Outcast(WordNet wordnet) {
-
+        this.wordnet = wordnet;
     }
 
     // given an array of WordNet nouns, return an outcast
     public String outcast(String[] nouns) {
-        return null;
+        String outcast = null;
+        int maxDist = 0;
+        for(int i=0; i<nouns.length; i++) {
+            int distance = 0;
+            for(int j=0; j<nouns.length; j++) {
+                if(i == j) {
+                    continue;
+                }
+
+                distance += wordnet.distance(nouns[i], nouns[j]);
+            }
+            if(maxDist == 0 || distance > maxDist) {
+                maxDist = distance;
+                outcast = nouns[i];
+            }
+        }
+        return outcast;
     }
 
     // for unit testing of this class (such as the one below)
--- wordnet/WordNet.java
+++ wordnet/WordNet.java
@@ -2,10 +2,10 @@ package wordnet;
 
 import algs4.Digraph;
 import stdlib.In;
+import stdlib.StdIn;
+import stdlib.StdOut;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * User: bharadwaj
@@ -14,50 +14,78 @@ import java.util.Map;
  */
 public class WordNet {
 
-    private Map<Integer, Integer []> wordTree = new HashMap<Integer, Integer []>();
+    private Map<Integer, Integer[]> wordTree = new HashMap<Integer, Integer[]>();
 
-    private Map<String, Integer> synsetMap = new HashMap<String, Integer>(); // synset string (all nouns) to its id
-    private Map<String, Integer> synsetNounMap = new HashMap<String, Integer>(); // synset induvidual nouns to its id
-    private Map<Integer, String> synsetIdMap = new HashMap<Integer, String>(); // synset id to single synset string of all nouns
+    public Map<String, Set<Integer>> synsetNounMap = new HashMap<String, Set<Integer>>(); // synset induvidual nouns to its id
+    public Map<Integer, String> synsetIdMap = new HashMap<Integer, String>(); // synset id to single synset string of all nouns
 
     private Digraph G;
     private SAP sap;
+
+    // A single synset like 'miracle' can have multiple IDs. 'miracle' has 54682 and 54683 (and maybe more!)
 
     // constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) throws java.lang.IllegalArgumentException {
         In synsetsIn = new In(synsets);
         String synsetsLine;
-        while(!synsetsIn.isEmpty()) {
+        int counter = 0;
+        while (!synsetsIn.isEmpty()) {
             synsetsLine = synsetsIn.readLine();
-            String [] synsetLineParts = synsetsLine.split(",");
-            Integer synsetId = Integer.parseInt(synsetLineParts[0]);
-            String [] nouns = synsetLineParts[1].split(" ");
+            //System.out.println("counter = " + counter++ + " line = " + synsetsLine);
 
-            synsetMap.put(synsetLineParts[1], synsetId);
+            String[] synsetLineParts = synsetsLine.split(",");
+            Integer synsetId = Integer.parseInt(synsetLineParts[0]);
+            //System.out.println("synsetId = " + synsetId);
+
+            //synsetMap.put(synsetLineParts[1], synsetId);
             synsetIdMap.put(synsetId, synsetLineParts[1]);
-            for(String noun: nouns) {
-                synsetNounMap.put(noun, synsetId);
+
+            String[] nouns = synsetLineParts[1].split(" ");
+            for (String noun : nouns) {
+                if (synsetNounMap.containsKey(noun)) {
+                    Set idSet = synsetNounMap.get(noun);
+                    idSet.add(synsetId);
+                } else {
+                    Set idSet = new HashSet<Integer>();
+                    idSet.add(synsetId);
+                    synsetNounMap.put(noun, idSet);
+                }
             }
         }
         synsetsIn.close();
 
-        G = new Digraph(synsetMap.size());
+        G = new Digraph(synsetIdMap.size());
+        System.out.println("# of vertices on the graph, synsetMap = " + synsetNounMap.size());
+        System.out.println("# of vertices on the graph, synsetIdMap = " + synsetIdMap.size());
 
         In hypernymsIns = new In(hypernyms);
         String hypernymsLine;
-        while(!hypernymsIns.isEmpty()) {
+        counter = 0;
+        while (!hypernymsIns.isEmpty()) {
             hypernymsLine = hypernymsIns.readLine();
-            String [] hypernymsParts = hypernymsLine.split(",");
+            String[] hypernymsParts = hypernymsLine.split(",");
             Integer synsetId = Integer.parseInt(hypernymsParts[0]);
 
-            Integer [] hypernymsArray = new Integer[hypernymsParts.length - 1];
-            for(int i=1; i<hypernymsParts.length-1; i++) {
-                hypernymsArray[i-1] = Integer.parseInt(hypernymsParts[i]);
-                G.addEdge(synsetId, hypernymsArray[i-1]);
+            Integer[] hypernymsArray = new Integer[hypernymsParts.length - 1];
+            if (hypernymsParts.length > 2) {
+                for (int i = 1; i < hypernymsParts.length - 1; i++) {
+                    hypernymsArray[i - 1] = Integer.parseInt(hypernymsParts[i]);
+                    G.addEdge(synsetId, hypernymsArray[i - 1]);
+                    //System.out.println("adding edge: " + synsetId + " => " + hypernymsArray[i - 1]);
+                    counter++;
+                }
+            } else if (hypernymsParts.length == 2) {
+                hypernymsArray[0] = Integer.parseInt(hypernymsParts[1]);
+                G.addEdge(synsetId, hypernymsArray[0]);
+                counter++;
+            } else {
+                System.out.println("flawed hypernym line = " + hypernymsLine);
             }
+
             wordTree.put(synsetId, hypernymsArray);
         }
         hypernymsIns.close();
+        System.out.println("number of edges added = " + counter);
 
         sap = new SAP(G);
     }
@@ -74,12 +102,12 @@ public class WordNet {
 
     // distance between nounA and nounB (defined below)
     public int distance(String nounA, String nounB) {
-        if(isNoun(nounA) == false || isNoun(nounB) == false) {
+        if (isNoun(nounA) == false || isNoun(nounB) == false) {
             throw new IllegalArgumentException("one of the nouns not part of wordnet = [" + nounA + ", " + nounB + "]");
         }
 
-        int nounAsynsetId = synsetNounMap.get(nounA);
-        int nounBsynsetId = synsetNounMap.get(nounB);
+        Set<Integer> nounAsynsetId = synsetNounMap.get(nounA);
+        Set<Integer> nounBsynsetId = synsetNounMap.get(nounB);
 
         return sap.length(nounAsynsetId, nounBsynsetId);
     }
@@ -87,12 +115,12 @@ public class WordNet {
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path (defined below)
     public String sap(String nounA, String nounB) {
-        if(isNoun(nounA) == false || isNoun(nounB) == false) {
+        if (isNoun(nounA) == false || isNoun(nounB) == false) {
             throw new IllegalArgumentException("one of the nouns not part of wordnet = [" + nounA + ", " + nounB + "]");
         }
 
-        int nounAsynsetId = synsetNounMap.get(nounA);
-        int nounBsynsetId = synsetNounMap.get(nounB);
+        Set<Integer> nounAsynsetId = synsetNounMap.get(nounA);
+        Set<Integer> nounBsynsetId = synsetNounMap.get(nounB);
 
         int ancestorId = sap.ancestor(nounAsynsetId, nounBsynsetId);
         return synsetIdMap.get(ancestorId);
@@ -100,12 +128,12 @@ public class WordNet {
 
     public class MyIterator implements Iterable<String>, Iterator<String> {
 
-        String[] nounArray = synsetMap.keySet().toArray(new String[0]);
+        String[] nounArray = synsetNounMap.keySet().toArray(new String[0]);
         int position = 0;
 
         @Override
         public boolean hasNext() {
-            if(position == nounArray.length - 1) {
+            if (position == nounArray.length - 1) {
                 return false;
             }
             return true;
@@ -129,14 +157,41 @@ public class WordNet {
     // for unit testing of this class
     public static void main(String[] args) {
         WordNet wordnet = new WordNet("./input/synsets.txt", "./input/hypernyms.txt");
-
         long counter = 0;
-        for(String noun: wordnet.nouns()) {
-            System.out.println(noun);
-            counter++;
-        }
+        //for(String noun: wordnet.nouns()) {
+        //    System.out.println(noun);
+        //    counter++;
+        //}
+        System.out.println("total num of nouns = " + counter);
 
-        System.out.println("total num of nouns = " + counter);
-        System.out.println("total num of nouns = " + counter);
+        while (!StdIn.isEmpty()) {
+            String nounA = StdIn.readString();
+            String nounB = StdIn.readString();
+
+            System.out.println("is A noun = " + wordnet.isNoun(nounA));
+            System.out.println("is B noun = " + wordnet.isNoun(nounB));
+
+            Set<Integer> idA = wordnet.synsetNounMap.get(nounA);
+            Set<Integer> idB = wordnet.synsetNounMap.get(nounB);
+
+            StringBuilder sb = new StringBuilder("");
+            for (Integer id : idA) {
+                sb.append(id);
+                sb.append(", ");
+            }
+            System.out.println("noun A ids = " + sb.toString());
+
+            sb = new StringBuilder("");
+            for (Integer id : idB) {
+                sb.append(id);
+                sb.append(", ");
+            }
+            System.out.println("noun B ids = " + sb.toString());
+
+
+            int length = wordnet.distance(nounA, nounB);
+            String ancestor = wordnet.sap(nounA, nounB);
+            StdOut.printf("length = %d, ancestor = %s\n", length, ancestor);
+        }
     }
 }
